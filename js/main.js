15a87abbd6838bef2f7b234c91799d15a9529466
Tyler Nielsen <tynielsen@gmail.com>
2012-12-22 19:42:54 -0700

    animating the humans

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(function() {
 
+  var guy = $('.humans-our-guy');
+  var lady = $('.humans-our-lady');
+
+  $(window).scroll(function() {
+    if(this.scrollY > 670) {
+      guy.removeClass('guy-fixed').addClass('guy-absolute');
+      lady.removeClass('lady-absolute-1').addClass('lady-fixed');
+    } else {
+      guy.removeClass('guy-absolute').addClass('guy-fixed');
+      lady.removeClass('lady-fixed').addClass('lady-absolute-1');
+    }
+  });
+
+});
