257563d75cc18228ba0d6159b720858ff4516bc5
Tyler Nielsen <tynielsen@gmail.com>
2012-12-22 22:46:32 -0700

    cleaning up js file

--- js/main.js
+++ js/main.js
@@ -5,14 +5,28 @@ $(function() {
 
   $(window).scroll(function() {
     if(this.scrollY > 670 && this.scrollY < 4721) {
-      guy.removeClass('guy-fixed').addClass('guy-absolute');
-      lady.removeClass('lady-absolute-1 lady-absolute-2').addClass('lady-fixed');
+
+      guy.removeClass('guy-fixed')
+         .addClass('guy-absolute');
+
+      lady.removeClass('lady-absolute-1 lady-absolute-2')
+          .addClass('lady-fixed');
+
     } else if(this.scrollY > 4720) {
-      guy.removeClass('guy-fixed').addClass('guy-absolute');
-      lady.removeClass('lady-fixed lady-absolute-1').addClass('lady-absolute-2');
+
+      guy.removeClass('guy-fixed')
+         .addClass('guy-absolute');
+
+      lady.removeClass('lady-fixed lady-absolute-1')
+          .addClass('lady-absolute-2');
+
     } else {
-      guy.removeClass('guy-absolute').addClass('guy-fixed');
-      lady.removeClass('lady-fixed lady-absolute-2').addClass('lady-absolute-1');
+
+      guy.removeClass('guy-absolute')
+         .addClass('guy-fixed');
+
+      lady.removeClass('lady-fixed lady-absolute-2')
+          .addClass('lady-absolute-1');
     }
   });
 
