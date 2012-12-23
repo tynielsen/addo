154221d03cbdf91f7599ff27440862f81765bb27
Tyler Nielsen <tynielsen@gmail.com>
2012-12-22 20:07:18 -0700

    adding in additional classname for lady for more exact position at bottom of page

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@ $(function() {
   var lady = $('.humans-our-lady');
 
   $(window).scroll(function() {
-    if(this.scrollY > 670) {
+    if(this.scrollY > 670 && this.scrollY < 4721) {
       guy.removeClass('guy-fixed').addClass('guy-absolute');
-      lady.removeClass('lady-absolute-1').addClass('lady-fixed');
+      lady.removeClass('lady-absolute-1 lady-absolute-2').addClass('lady-fixed');
+    } else if(this.scrollY > 4720) {
+      guy.removeClass('guy-fixed').addClass('guy-absolute');
+      lady.removeClass('lady-fixed lady-absolute-1').addClass('lady-absolute-2');
     } else {
       guy.removeClass('guy-absolute').addClass('guy-fixed');
-      lady.removeClass('lady-fixed').addClass('lady-absolute-1');
+      lady.removeClass('lady-fixed lady-absolute-2').addClass('lady-absolute-1');
     }
   });
 
