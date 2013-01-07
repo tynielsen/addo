bacbceeb8c09cf1a36b9b7ca0bfdd964b9a007c9
Tyler Nielsen <tynielsen@gmail.com>
2013-01-07 00:24:26 -0700

    tweaking styles, working on fading in the text as the person slides by

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ $(function() {
   var guy = $('.humans-our-guy');
   var lady = $('.humans-our-lady');
 
+  $('.slide-1 p').fadeIn(1500);
+
   $(window).scroll(function() {
+    //tracking the movement of the people
     if(this.scrollY > 670 && this.scrollY < 4721) {
 
       guy.removeClass('guy-fixed')
@@ -27,6 +30,29 @@ $(function() {
 
       lady.removeClass('lady-fixed lady-absolute-2')
           .addClass('lady-absolute-1');
+    }
+
+    // fading in the paragraphs when they are on screen
+    if(this.scrollY > 536) {
+      $('.slide-2 p').fadeIn('slow');
+    }
+    if(this.scrollY > 1145) {
+      $('.slide-3 p').fadeIn('slow');
+    }
+    if(this.scrollY > 2006) {
+      $('.slide-4 p').fadeIn('slow');
+    }
+    if(this.scrollY > 2670) {
+      $('.slide-5 p').fadeIn('slow');
+    }
+    if(this.scrollY > 3325) {
+      $('.slide-6 p').fadeIn('slow');
+    }
+    if(this.scrollY > 4010) {
+      $('.slide-7 p').fadeIn('slow');
+    }
+    if(this.scrollY > 4731) {
+      $('.slide-8 p').fadeIn('slow');
     }
   });
 
