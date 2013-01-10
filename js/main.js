a273bf53464bf8378f880c341fdfc702c7d4976f
Tyler Nielsen <tynielsen@gmail.com>
2013-01-09 23:10:10 -0700

    troubleshooting stupid IE

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@ $(function() {
 
   $('.slide-1 p').fadeIn(1500);
 
-  if($(window).height() > 780) {
+  if($(window).height() > '680px') {
     $('.main-content').css('padding-bottom', '300px');
   }
 
   $(window).scroll(function() {
+    var scrolledDown = this.scrollY;
+
     //tracking the movement of the people
-    if(this.scrollY > 670 && this.scrollY < 4711) {
+    if(scrolledDown > 670 && scrolledDown < 4711) {
 
       guy.removeClass('guy-fixed')
          .addClass('guy-absolute');
@@ -19,7 +21,7 @@ $(function() {
       lady.removeClass('lady-absolute-1 lady-absolute-2')
           .addClass('lady-fixed');
 
-    } else if(this.scrollY > 4710) {
+    } else if(scrolledDown > 4710) {
 
       guy.removeClass('guy-fixed')
          .addClass('guy-absolute');
@@ -37,25 +39,25 @@ $(function() {
     }
 
     // fading in the paragraphs when they are on screen
-    if(this.scrollY > 595) {
+    if(scrolledDown > 595) {
       $('.slide-2 p').fadeIn('fast');
     }
-    if(this.scrollY > 1290) {
+    if(scrolledDown > 1290) {
       $('.slide-3 p').fadeIn('fast');
     }
-    if(this.scrollY > 1850) {
+    if(scrolledDown > 1790) {
       $('.slide-4 p').fadeIn('fast');
     }
-    if(this.scrollY > 2670) {
+    if(scrolledDown > 2670) {
       $('.slide-5 p').fadeIn('fast');
     }
-    if(this.scrollY > 3335) {
+    if(scrolledDown > 3335) {
       $('.slide-6 p').fadeIn('fast');
     }
-    if(this.scrollY > 4015) {
+    if(scrolledDown > 4015) {
       $('.slide-7 p').fadeIn('fast');
     }
-    if(this.scrollY > 4630) {
+    if(scrolledDown > 4630) {
       $('.slide-8 p').fadeIn('fast');
       
       //show view more arrow only if it isn't already showing, don't want this to keep firing
